da43a5a5ba6b9345e30eb1a23fa81c6592810e58
angelamazakas8 <angela.mazakas60@gmail.com>
2024-07-31 22:19:03 -0700

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,4 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4.0,
-        count: 127
-    },
-    priceCents: 2095   
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799  
-}];
+// uses product data from data/products.js
 
 let productsHTML = '';
 
@@ -79,6 +54,5 @@ products.forEach((product) => {
         </div>`
 });
 
-console.log(productsHTML);
 
 document.querySelector('.products-grid').innerHTML = productsHTML;
