be72dee75284602eb5bd8fd4c83e5da82dbb300d
angelamazakas8 <angela.mazakas60@gmail.com>
2024-08-02 22:49:20 -0700

    cart now uses localStorage to save cart

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,21 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
 }];
+
+}
+
+
+// save cart to local storage, so it doesn't change when page is refreshed
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -27,6 +38,8 @@ export function addToCart(productId) {
       cart.push({productId, quantity: 1});
 
     }
+    // once item is added to cart, save cart to local storage
+    saveToStorage();
 }
 
 export function updateCartQuantity(){
@@ -59,4 +72,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+  // save updated cart to local storage
+  saveToStorage();
 }
